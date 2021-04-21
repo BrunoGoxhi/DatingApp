c6332c34aeeb8fc4f788c984f4f9614c98270772
bgoxhi <bruno_goxhi@hotmail.com>
2021-04-21 23:19:48 +0200

    End of section 5 (Register Login ClientSide)

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,14 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  model: any = {  }
-  constructor() { }
+  model: any = {}
+  constructor(public accountService: AccountService) { }
 
-  ngOnInit(): void { 
-  }
+  ngOnInit(): void { }
 
   login()
   {
-    console.log(this.model)
+    this.accountService.login(this.model).subscribe(request => {
+      console.log(request);
+    }, error =>{
+      console.log(error);
+    });
   }
+
+  logout()
+  {
+    this.accountService.logout();
+  }
+  
 }
